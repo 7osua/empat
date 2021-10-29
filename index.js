7459e1f84b38d57ca97a46ff6aa9bf4be182fb05
Josua Manullang <j7.manullang@gmail.com>
2021-10-29 17:18:48 +0700

    [^] menambah navigation dengan js + style untuk navigation

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-const infos = ['Budgeting', 'Allocating', 'Personal Finance', 'Expenses', 'Savings', 'Debt', 'Planning'];
+const root = document.getElementById('root');
+const writeHeader = function () {
+    document.body.insertAdjacentHTML(
+        'beforeend',
+        `
+            <header class="head">
+                  <div class="head-logo">
+                        <img class="head-logo-image"
+                             src="favicon_io/android-chrome-192x192.png"
+                             alt="04.empat">
+                        <h1 class="head-logo-label">empat</h1>
+                  </div>
+            </header>
+    `,
+    );
+};
 
-infos.forEach((item)=>{
-    console.log(item);
-});
+const writeNavigation = function () {
+    document.body.insertAdjacentHTML(
+        'beforeend',
+        `
+            <nav class="navigation">
+                  <div class="navigation-container">
+                        <button class='navigation-drawer'>
+                              <span class="navigation-drawer-icon material-icons-round"> menu </span>
+                        </button>
+                        <h2 class="navigation-title">Travel & Bugdet</h2>
+                        <button class="navigation-drawer">
+                              <span class="navigation-drawer-icon material-icons-round"> more_vert </span>
+                        </button>
+                  </div>
+            </nav>
+    `,
+    );
+};
+
+const initPage = function(){
+  writeHeader();
+  writeNavigation();  
+};
+
+window.onload = initPage();
